d9dbbab7cacc971627494dba56c72d51b326c60b
Jefferson <thesteelninjacode@gmail.com>
2024-03-13 23:56:45 -0600

    Update package version and add new methods to IModel interface

--- dist/src/lib/prisma/model/IModel.php
+++ dist/src/lib/prisma/model/IModel.php
@@ -5,15 +5,16 @@ namespace Lib\Prisma\Model;
 interface IModel
 {
     public function create(array $data);
+    public function createMany(array $data);
     public function findUnique(array $criteria);
-    public function findMany(array $select);
+    public function findMany(array $criteria);
     public function findFirst(array $criteria);
     public function update(array $data);
+    public function updateMany(array $data);
     public function delete(array $criteria);
+    public function deleteMany(array $criteria);
     public function upsert(array $data);
     public function aggregate(array $operation);
     public function groupBy(array $by);
-    public function updateMany(array $data);
-    public function deleteMany(array $criteria);
     public function count(array $criteria);
 }
