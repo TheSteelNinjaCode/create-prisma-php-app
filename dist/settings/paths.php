be8adc5bceb326cdadd8d0751f05758e6645f5b1
Jefferson <thesteelninjacode@gmail.com>
2024-04-13 11:38:11 -0600

    Update package version to 1.10.20 in package.json and remove unused API_PATH constant in paths.php

--- dist/settings/paths.php
+++ dist/settings/paths.php
@@ -6,6 +6,5 @@ define("PUBLIC_PATH", dirname(SETTINGS_PATH) . "/public");
 define("PRISMA_LIB_PATH", dirname(SETTINGS_PATH) . "/src/Lib/Prisma");
 define("SRC_PATH", dirname(SETTINGS_PATH) . "/src");
 define("APP_PATH", dirname(SETTINGS_PATH) . "/src/app");
-define("API_PATH", dirname(SETTINGS_PATH) . "/src/app/api");
 define("LIB_PATH", dirname(SETTINGS_PATH) . "/src/Lib");
 define("DOCUMENT_PATH", dirname(SETTINGS_PATH));
