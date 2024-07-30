6b7da1cb2116ff06af42d9cc3ade3cee9f38fb1c
TheSteelNinjaCode <thesteelninjacode@gmail.com>
2024-07-30 01:01:54 -0600

    chore: Update package version to 1.16.25 in package.json and add pp-description meta tag in layout.php

--- dist/src/app/layout.php
+++ dist/src/app/layout.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?php echo htmlspecialchars($metadata['description']); ?>">
+    <meta name="pp-description" content="<?php echo htmlspecialchars($metadata['description']); ?>">
     <title><?php echo htmlspecialchars($metadata['title']); ?></title>
     <link rel="shortcut icon" href="<?php echo $baseUrl; ?>favicon.ico" type="image/x-icon">
 </head>
