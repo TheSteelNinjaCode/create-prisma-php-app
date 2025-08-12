253ea51626b4fa209b72351c9cbb53849322089f
TheSteelNinjaCode <thesteelninjacode@gmail.com>
2025-08-12 02:48:52 -0600

    Implement code changes to enhance functionality and improve performance

--- dist/src/app/index.php
+++ dist/src/app/index.php
@@ -1,16 +1,18 @@
-<?php use Lib\Request; ?>
+<?php
+
+use Lib\Request; ?>
 
 <div class="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
     <main class="flex flex-col gap-5 row-start-2 items-center sm:items-center">
         <h1 class="text-6xl font-bold tracking-tight">
-            <span class="text-black">PRISMA</span>
-            <span class="text-black text-2xl align-super">PHP</span>
+            <span class="text-foreground">PRISMA</span>
+            <span class="text-foreground text-2xl align-super">PHP</span>
         </h1>
 
         <ol class="list-inside list-decimal text-sm/6 text-center sm:text-left">
             <li class="mb-2 tracking-[-.01em]">
                 Get started by editing
-                <code class="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+                <code class="bg-text-foreground/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
                     src/app/index.php
                 </code>
                 .
@@ -26,7 +28,24 @@
                 href="https://prismaphp.tsnc.tech/docs"
                 target="_blank"
                 rel="noopener noreferrer">
-                <img src="<?= Request::baseUrl ?>/assets/images/prisma-php-black.svg" alt="Read Docs" width="20" height="20" style="filter: invert(1) sepia(1) hue-rotate(180deg);" /> Read Docs
+                <svg
+                    version="1.1"
+                    viewBox="0 0 123.8 126.8"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    aria-hidden="true">
+                    <g>
+                        <g id="Layer_2">
+                            <g id="Layer_1-2">
+                                <polygon
+                                    points="113.9 50.6 111.5 49.4 105 46.2 97.1 42.4 92.7 37 89 32.7 84.6 31.3 77.9 32.6 72.4 37.9 71.3 45.5 68.1 50.9 66.2 46.6 64.6 33.1 63.1 21.6 60.3 15.1 50.4 5.5 46.7 20.5 46.7 32.9 39 27.4 24.1 11.8 0 0 6.5 12.1 18.2 24.7 27.8 42.1 41 55.6 47.7 66.2 45.2 73.5 44.1 89 35.8 99.3 34.1 106.8 39.7 105.2 37.1 113.5 39.4 122.5 44.1 116.3 47.7 126.8 53.4 117.9 60.8 123.2 62.2 113.2 68.3 115.3 67.8 108.3 74.2 110.1 72.7 101.7 58.8 90.1 77 80.7 84 72.3 88 62.6 90.8 51.1 94.1 46.6 99.8 47.3 107.2 49.9 112.2 51.6 123.8 58.2 113.9 50.6" />
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+                Read Docs
             </a>
         </div>
     </main>
