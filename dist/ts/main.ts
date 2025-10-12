2dabe2db301de4a3bfb37f124af8cb5609b320d4
TheSteelNinjaCode <thesteelninjacode@gmail.com>
2025-10-11 22:16:24 -0600

    chore: update version to 4.1.0-alpha.17 in package.json; adjust import paths in main.ts

--- dist/ts/main.ts
+++ dist/ts/main.ts
@@ -1,2 +1,7 @@
-import "../public/js/pp-reactive-v1.js";
-import "../public/js/pp-utilities.js";
+import "./pp-reactive-v1.js";
+import "./pp-utilities.js";
+
+// The following global names have already been declared elsewhere in the project:
+// - pp: Used for the Reactive Core functionality.
+// - store: Handles local storage synchronization with the server.
+// - searchParams: Manages URL manipulation and query parameters.
